18d29e44cf2a5d0d16599ae6cd39729851908db4
miu <ozakimiu0506@gmail.com>
2024-04-06 16:27:09 +0900

    シーディング修正

--- src/database/seeders/AttendancesTableSeeder.php
+++ src/database/seeders/AttendancesTableSeeder.php
@@ -15,35 +15,35 @@ class AttendancesTableSeeder extends Seeder
     public function run()
     {
         $time = [
-            'user_id' => 31,
+            'user_id' => 1,
             'date' => '2024-04-01',
             'work_start' => '10:00:00',
             'work_end' => '20:00:00'
         ];
         DB::table('attendances')->insert($time);
         $time = [
-            'user_id' => 32,
+            'user_id' => 2,
             'date' => '2024-04-01',
             'work_start' => '10:00:10',
             'work_end' => '20:00:00'
         ];
         DB::table('attendances')->insert($time);
         $time = [
-            'user_id' => 33,
+            'user_id' => 3,
             'date' => '2024-04-01',
             'work_start' => '10:00:10',
             'work_end' => '20:00:00'
         ];
         DB::table('attendances')->insert($time);
         $time = [
-            'user_id' => 34,
+            'user_id' => 4,
             'date' => '2024-04-01',
             'work_start' => '10:00:20',
             'work_end' => '20:00:00'
         ];
         DB::table('attendances')->insert($time);
         $time = [
-            'user_id' => 35,
+            'user_id' => 5,
             'date' => '2024-04-01',
             'work_start' => '10:00:20',
             'work_end' => '20:00:00'
--- src/database/seeders/RestsTableSeeder.php
+++ src/database/seeders/RestsTableSeeder.php
@@ -15,31 +15,31 @@ class RestsTableSeeder extends Seeder
     public function run()
     {
         $time = [
-            'attendance_id' => 3,
+            'attendance_id' => 1,
             'rest_start' => '00:30:00',
             'rest_end' => '09:30:00'
         ];
         DB::table('rests')->insert($time);
         $time = [
+            'attendance_id' => 2,
+            'rest_start' => '00:30:00',
+            'rest_end' => '09:29:50'
+        ];
+        DB::table('rests')->insert($time);
+        $time = [
+            'attendance_id' => 3,
+            'rest_start' => '00:30:00',
+            'rest_end' => '09:29:50'
+        ];
+        DB::table('rests')->insert($time);
+        $time = [
             'attendance_id' => 4,
-            'rest_start' => '00:30:00',
-            'rest_end' => '09:29:50'
-        ];
-        DB::table('rests')->insert($time);
-        $time = [
-            'attendance_id' => 5,
-            'rest_start' => '00:30:00',
-            'rest_end' => '09:29:50'
-        ];
-        DB::table('rests')->insert($time);
-        $time = [
-            'attendance_id' => 6,
             'rest_start' => '00:30:00',
             'rest_end' => '09:29:40'
         ];
         DB::table('rests')->insert($time);
         $time = [
-            'attendance_id' => 7,
+            'attendance_id' => 5,
             'rest_start' => '00:30:00',
             'rest_end' => '09:29:40'
         ];
